a62ece55355c3c78a917878cd62df70f7cf57b3e
Geordee Naliyath <geordee@gmail.com>
2014-03-31 11:25:30 +0530

    Fix member new error

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,17 +1,17 @@
 class MembersController < MastersController
   def new
-    @member = Member.new
-    @member.build_address
+    @master = Member.new
+    @master.build_address
   end
 
   def edit
-    @member.build_address unless @member.address
+    @master.build_address unless @master.address
   end
 
 private
 
   def master_params
-    params.require(:member).permit(:membership_id, :code, :name, :kind, :gender, :age_group, :notes,
+    params.require(:master).permit(:membership_id, :code, :name, :kind, :gender, :age_group, :notes,
                                    address_attributes: [:id, :line_1, :line_2, :landmark, :city,
                                                         :state, :country, :postal_code, :mobile,
                                                         :phone_1, :phone_2, :email_1, :email_2])
