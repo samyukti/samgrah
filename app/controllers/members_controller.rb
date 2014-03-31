9559a823f50dfa86cda583643acc3fffab7cbeb4
Geordee Naliyath <geordee@gmail.com>
2014-03-31 11:48:48 +0530

    Another fix on members controller

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,7 +11,7 @@ class MembersController < MastersController
 private
 
   def master_params
-    params.require(:master).permit(:membership_id, :code, :name, :kind, :gender, :age_group, :notes,
+    params.require(:member).permit(:membership_id, :code, :name, :kind, :gender, :age_group, :notes,
                                    address_attributes: [:id, :line_1, :line_2, :landmark, :city,
                                                         :state, :country, :postal_code, :mobile,
                                                         :phone_1, :phone_2, :email_1, :email_2])
