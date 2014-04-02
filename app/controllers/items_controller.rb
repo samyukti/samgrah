49b5050bbd873b105271f6048d7c1d690c2f0cf3
Geordee Naliyath <geordee@gmail.com>
2014-04-02 18:25:20 +0530

    Add format to strong params

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -11,7 +11,7 @@ class ItemsController < MastersController
 private
 
   def item_params
-    params.require(:item).permit(:category_id, :code, :name, :subject, :kind, :length, :length_uom,
+    params.require(:item).permit(:category_id, :code, :name, :subject, :kind, :format, :length, :length_uom,
                                  :publisher, :published_date, :language, :isbn_10, :isbn_13, :rights,
                                  :tags, :excerpt, :rating, :photo, :remote_photo_url, :notes,
                                  item_creators_attributes: [:id, :sequence, :creator_id, :role, :_destroy])
