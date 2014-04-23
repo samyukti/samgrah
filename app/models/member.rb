b299369bb3712d82e630929c7ff142ac639ab29d
Geordee Naliyath <geordee@gmail.com>
2014-04-23 20:15:43 +0530

    Simple validations for enrolment in member model

--- app/models/member.rb
+++ app/models/member.rb
@@ -13,4 +13,18 @@ class Member < ActiveRecord::Base
 
   validates_presence_of :code, :name, :kind, :membership_id
   validates_uniqueness_of :code
+  validate :enrolment_dates
+
+  private
+
+  def enrolment_dates
+    errors.add(:closed_date, 'Closed date should be greater than or equal to join date.') \
+      if self.join_date && self.closed_date && self.closed_date < self.join_date
+    errors.add(:renewal_date, 'Renewal date should be greater than or equal to last renewed date.') \
+      if self.renewal_date && self.renewed_date && self.renewal_date < self.renewed_date
+    errors.add(:renewed_date, 'Renewal date should be greater than or equal to join date.') \
+      if self.join_date && self.renewed_date && self.renewed_date < self.join_date
+    errors.add(:renewal_date, 'Renewal date should be greater than or equal to join date.') \
+      if self.join_date && self.renewal_date && self.renewal_date < self.join_date
+  end
 end
--- spec/models/member_spec.rb
+++ spec/models/member_spec.rb
@@ -21,4 +21,20 @@ describe Member do
   it 'should have a membership' do
     build(:member, membership_id: nil).should_not be_valid
   end
+
+  it 'should not allow a closed date earlier than join date' do
+    build(:member, join_date: Date.today, closed_date: Date.today - 1).should_not be_valid
+  end
+
+  it 'should not allow renewal earlier than join date' do
+    build(:member, join_date: Date.today, renewed_date: Date.today - 1).should_not be_valid
+  end
+
+  it 'should not allow next renewal date earlier than join date' do
+    build(:member, join_date: Date.today, renewal_date: Date.today - 1).should_not be_valid
+  end
+
+  it 'should not allow next renewal earlier than last renewal' do
+    build(:member, renewed_date: Date.today, renewal_date: Date.today - 1).should_not be_valid
+  end
 end
